ca8d2c40bfbeafbb1e171306fc41b2426d9c13cf
mrawesomeac@gmail.com <mrawesomeac@gmail.com>
2023-11-27 11:09:51 -0500

    Working on Reciept class

---
+++ src/main/java/com/pluralsight/Receipt.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+import java.util.*;
+import java.io.*;
+
+
+public class Receipt {
+    public static void main(String[] args) {
+        String filePath = "Receipt.csv";
+    }
+}
