d3074fe228d132b793bd5dbabfc7a40c667c97b8
mrawesomeac@gmail.com <mrawesomeac@gmail.com>
2023-11-27 11:31:51 -0500

    Finished first reader

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -1,10 +1,25 @@
 package com.pluralsight;
 import java.util.*;
 import java.io.*;
-
+import java.time.*;
 
 public class Receipt {
     public static void main(String[] args) {
         String filePath = "Receipt.csv";
+
+        try{
+            BufferedReader reader= new BufferedReader((new FileReader(filePath)));
+
+            String line;
+            while ((line = reader.readLine()) != null){
+
+                System.out.println(line);
+            }
+                reader.close();
+        }catch (IOException e){
+            e.getStackTrace();
+
+
     }
+}
 }
