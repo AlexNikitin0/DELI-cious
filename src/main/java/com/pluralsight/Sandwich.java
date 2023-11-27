459e61ea5780c7ce01a00d8d75c055f78b571e86
anikiti3 <anikiti3@email.cpcc.edu>
2023-11-27 11:04:20 -0500

    created sandwich class

---
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Sandwich {
+    //properties
+    private int size; // size of sandwich in inches. Options: 4,8,12
+    private String bread; // options: white,wheat,rye, or wrap
+
+
+}
