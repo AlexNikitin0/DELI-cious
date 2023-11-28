5d1ba0756272ce293239910e937b5f6855ddddf5
Desi Rorie <desirorie@Desis-MBP.lan>
2023-11-28 09:34:20 -0500

    touch ups

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -74,56 +74,43 @@ class OrderMenu {
                     boolean extraMeat;
                     boolean extraCheese;
 
-                    List<String> meatToppings = Arrays.asList("Steak","Ham", "Salami", "Roast Beef","Chicken","Bacon");
+                    List<String> meatToppings = Arrays.asList("Steak","Ham", "Salami", "Roast Beef","Chicken","Bacon","No Meat");
                     List<String> cheeseToppings = Arrays.asList("American","Provolone", "Cheddar", "Swiss","Pepperjack");
                     List<String> vegToppings = Arrays.asList("Lettuce","Peppers", "Onions", "Tomatoes","Jalapenos","Cucumbers","Pickles","Guacamole","Mushrooms");
                     List<String> sideToppings = Arrays.asList("Au Jus","Sauce");
-
                     Sandwich sandwich = new Sandwich(size,bread,bool);
-                    customerOrder.sandwiches.add(sandwich);
+
 
                     //Meat Choice
-
-
                     int moreMeatToppings = 1;
 
-                    while(moreMeatToppings == 1){
+                    while(moreMeatToppings != 7){
+                        if (moreMeatToppings <= 6){
+                            System.out.println("These are our meat toppings");
+                            for( int i = 1; i < meatToppings.size() + 1 ; i++){
+                                System.out.println(i+ ":" + meatToppings.get(i - 1));
+                            }
 
-                        System.out.println("These are our meat toppings");
-                        for( int i = 1; i < meatToppings.size() + 1 ; i++){
-                            System.out.println(i+ ":" + meatToppings.get(i - 1));
+                             moreMeatToppings = order.nextInt();
+                            order.nextLine();
+
+                            sandwich.toppings.add(meatToppings.get(moreMeatToppings - 1 ));
                         }
-
-                        int meatChoice = order.nextInt();
-                        order.nextLine();
-
-                        sandwich.toppings.add(meatToppings.get(meatChoice - 1 ));
-
-                        System.out.println("Do you want more meat toppings?");
-                        System.out.println("1: Yes");
-                        System.out.println("2: No");
-
-                        moreMeatToppings = order.nextInt();
-                        order.nextLine();
-
                     }
-
-
-
                     //Cheese Choice
                     int moreCheeseToppings= 1;
 
                     while(moreCheeseToppings == 1){
 
-                        System.out.println("These be the cheeses!!gt");
+                        System.out.println("These be the cheeses!!");
                         for( int i = 1; i <cheeseToppings.size() + 1 ; i++){
                             System.out.println(i+ ":" + cheeseToppings.get(i - 1));
                         }
 
-                        int cheeseChoice = order.nextInt();
+                        moreCheeseToppings = order.nextInt();
                         order.nextLine();
 
-                        sandwich.toppings.add(meatToppings.get(cheeseChoice- 1 ));
+                        sandwich.toppings.add(meatToppings.get(moreCheeseToppings - 1 ));
 
                         System.out.println("Do you want more cheese toppings?");
                         System.out.println("1: Yes");
@@ -133,11 +120,6 @@ class OrderMenu {
                         order.nextLine();
 
                     }
-
-
-
-
-
                     //Vegetable Choice
                     int moreVegetableToppings = 1;
 
@@ -148,10 +130,10 @@ class OrderMenu {
                             System.out.println(i+ ":" + vegToppings.get(i - 1));
                         }
 
-                        int vegChoice = order.nextInt();
+                        moreVegetableToppings = order.nextInt();
                         order.nextLine();
 
-                        sandwich.toppings.add(meatToppings.get(vegChoice - 1 ));
+                        sandwich.toppings.add(meatToppings.get(moreVegetableToppings - 1 ));
 
                         System.out.println("Do you want more vegetable toppings?");
                         System.out.println("1: Yes");
@@ -159,15 +141,7 @@ class OrderMenu {
 
                         moreVegetableToppings = order.nextInt();
                         order.nextLine();
-
                     }
-
-
-
-
-
-
-
                     //Sauce choice
                     int moreSauces = 1;
 
@@ -178,10 +152,10 @@ class OrderMenu {
                             System.out.println(i+ ":" + sideToppings.get(i - 1));
                         }
 
-                        int sauceChoice = order.nextInt();
+                        moreSauces = order.nextInt();
                         order.nextLine();
 
-                        sandwich.toppings.add(sideToppings.get(sauceChoice - 1 ));
+                        sandwich.toppings.add(sideToppings.get(moreSauces - 1 ));
 
                         System.out.println("Do you want more vegetable toppings?");
                         System.out.println("1: Yes");
@@ -191,8 +165,7 @@ class OrderMenu {
                         order.nextLine();
 
                     }
-
-
+                    customerOrder.sandwiches.add(sandwich);
                     break;
                 case "2":
                     addDrink();
