41af3c2e34b927b9148f2eb32a66f5882f5ea898
Christian Crites <ccrites98@gmail.com>
2023-11-28 10:30:17 -0500

    Signature

--- src/main/java/com/pluralsight/SignatureSandwich.java
+++ src/main/java/com/pluralsight/SignatureSandwich.java
@@ -1,2 +1,21 @@
-package com.pluralsight;public class SignatureSandwich {
+package com.pluralsight;
+import java.util.Arrays;
+import java.util.List;
+public class SignatureSandwich extends Sandwich {
+    public SignatureSandwich(int size, String bread, boolean toasted, List<String> toppings) {
+        super(size, bread, toasted);
+        addToppings(toppings);
+    }
+    private void addToppings(List<String> toppings) {
+    }
+}
+class BLTSandwich extends SignatureSandwich {
+    public BLTSandwich() {
+        super(8, "White", true, Arrays.asList("Bacon", "Cheddar", "Lettuce", "Tomato", "Ranch"));
+    }
+}
+class PhillyCheeseSteak extends SignatureSandwich {
+    public PhillyCheeseSteak() {
+        super(8, "White", true, Arrays.asList("Steak", "American", "Peppers", "Mayo", "Toasted"));
+    }
 }
