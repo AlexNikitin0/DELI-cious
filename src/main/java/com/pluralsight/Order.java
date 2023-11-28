5cdd3d900a3ca5a9d5efdef6bc56f08d81e6ac8d
Christian Crites <ccrites98@gmail.com>
2023-11-28 08:52:42 -0500

    fixing

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,9 +1,9 @@
 package com.pluralsight;
+
 import java.util.ArrayList;
 
 public class Order {
     double total;
-
 
     ArrayList<Sandwich> sandwiches;
     public Order() {
@@ -12,37 +12,7 @@ public class Order {
 
         //  getTotal();
     }
-    ArrayList<Sandwich> sandwiches;
-    public Order() {
-        this.total = 0;
-        this.sandwiches = new ArrayList<Sandwich>();
 
-      //  getTotal();
-    }
-
-
-    public void addSandwich(Sandwich sandwich){
-
-        sandwiches.add(sandwich);
-
-    }
-    public void getTotal(){
-        for (Sandwich sandwich: sandwiches) {
-            this.total += sandwich.getTotalPrice();
-        }
-    }
-
-    public void addDrink(String size){
-        if (size.equalsIgnoreCase("small")){
-            this.total += 2;
-        } else if (size.equalsIgnoreCase("medium")) {
-            this.total += 2.50;
-        } else {
-            this.total += 3.00;
-        }
-
-
-    }
 
     public void addSandwich(Sandwich sandwich){
 
